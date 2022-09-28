c45b5f385a7741d242499cd6dd500179727865e3
RaulMonPi <al428109@uji.es>
2022-09-28 18:32:29 +0200

    Update SpawnManager.cs

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -2,24 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
-{
-    public GameObject[] obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
-    private int randomObstacle;
-
-    //Variables para determinar el casteo y enfriamiento del Invoke.
-    private float startDelay = 2;
-    private float repeatRate = 2;
-
-    //Variable de referencia de scripts externos
-    private PlayerController playerCScript;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerCScript = GameObject.Find("Player").GetComponent<PlayerController>();
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+public class SpawnManager : MonoBehatRate);
     }
 
     // Update is called once per frame
